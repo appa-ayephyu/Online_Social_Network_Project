6d6d27c8d9692e5df88e3a16b5df355ae4975e43
unknown <nick.zhang@clefintelligence.com>
2019-10-08 01:07:03 +0800

    modified lazy algorithm

--- DIC_Greedy/DIC_Greedy_Algorithm.py
+++ DIC_Greedy/DIC_Greedy_Algorithm.py
@@ -4,6 +4,7 @@ import random
 import copy
 import time
 from xml.dom import minidom
+import heapq
 
 
 def read_xml_file(path):
@@ -91,7 +92,7 @@ def greedy(G, B, ap, pp, mc):
     return S, max_expected_spread
 
 
-def a_greedy(G, B, ap, pp, mc):
+def lazy_greedy(G, B, ap, pp, mc):
     """
     :param G: Graph
     :param B: Budget
@@ -116,29 +117,84 @@ def a_greedy(G, B, ap, pp, mc):
     spread = spread_list[0][1]
     spread_list.pop(0)
 
-    spread_list_copy = spread_list[:]
-
+    # spread_list_copy = spread_list[:]
     while B-len(S):
         # current_best_node = []
+        sorted_spread = spread_list[1:]
         for i in range(len(spread_list)):
             s = spread_list[i]
+            position_check = 0
             expected_spread = DIC(G, S, [s[0]], ap, pp, mc)
             marginal_gain = expected_spread - spread
-            if marginal_gain >= spread_list_copy[0][1]:
-                S.append(spread_list_copy[0][0])
-                spread_list_copy.pop(0)
+            if s[0] == sorted_spread[0][0]:
+                position_check = 1
+
+            if marginal_gain >= sorted_spread[position_check][1]:
+                S.append(s[0])
+                spread_list.pop(i)
                 spread = expected_spread
                 break
             else:
-                spread_list_copy[i][1] = marginal_gain
-                spread_list_copy.sort(key=lambda x: x[1], reverse=True)
+                spread_list[i][1] = marginal_gain
+                sorted_spread = spread_list[:]
+                sorted_spread.sort(key=lambda x:x[1], reverse=True)
 
-        spread_list = spread_list_copy[:]
+        spread_list.sort(key=lambda x: x[1], reverse=True)
+
+    return S, spread
+
+def lazy_greedy_2(G, B, ap, pp, mc):
+    """
+    :param G: Graph
+    :param B: Budget
+    :param ap: Activation Probability for seed node
+    :param pp: Propagation Probability
+    :param mc: Number of Monte-Carlo simulations
+    :return: max spread
+    """
+
+    S = [] # initial seed-set
+    all_nodes = list(G.nodes)
+
+    spread_list = []
+
+    for s in list(set(all_nodes) - set(S)):
+        # print('new s is: {} and S is {}'.format(s, S))
+        expected_spread = DIC(G, S, [s], ap, pp, mc)
+        heapq.heappush(spread_list, (-expected_spread, s))
+
+    spread, seed = heapq.heappop(spread_list)
+    spread = -spread
+
+    S.append(seed)
+    spread_list.pop(0)
+
+    # spread_list_copy = spread_list[:]
+    while B-len(S):
+        # current_best_node = []
+        found_best_seed = False
+        node_lookup = 0
+        while not found_best_seed:
+            node_lookup += 1
+            _, potential_seed = heapq.heappop(spread_list)
+            expected_spread = DIC(G, S, [potential_seed], ap, pp, mc)
+            marginal_gain = expected_spread - spread
+            heapq.heappush(spread_list, (-marginal_gain, potential_seed))
+
+            if spread_list[0][1] == potential_seed:
+                found_best_seed = True
+
+        spread, seed = heapq.heappop(spread_list)
+        spread = -spread
+        S.append(seed)
+        print(node_lookup)
 
     return S, spread
 
 
 G = read_xml_file('N_2500_beta_1.2_01.xml')
 print(nx.info(G))
-print(a_greedy(G, 5, 1.0, 0.01, 100))
-print(greedy(G, 1, 1.0, 0.01, 100))
+start_time = time.time()
+print(lazy_greedy_2(G, 5, 1.0, 0.01, 1500))
+print("took {} seconds to run".format(time.time()-start_time))
+# print(greedy(G, 1, 1.0, 0.01, 10000))
